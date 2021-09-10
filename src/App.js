b78faef5aba0efb89083d43f84e75581faa26137
Kirill Gelazhev <kgelazhev@gmail.com>
2021-09-10 13:05:23 +0300

    initializing project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles(theme => ({
+	root: {
+		flexGrow: 1,
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		paddingTop: '100px',
+		width: '500px',
+		minHeight: '500px',
+	},
+	paper: {
+		border: '1px solid',
+		maxHeight: '150px',
+		borderRadius: 0,
+		padding: theme.spacing(10),
+		textAlign: 'center',
+		color: theme.palette.text.secondary,
+	},
+}))
+
+export default function NestedGrid() {
+	const classes = useStyles()
+
+	function FormRow() {
+		return (
+			<React.Fragment>
+				<Grid item xs={4}>
+					<Paper className={classes.paper}></Paper>
+				</Grid>
+				<Grid item xs={4}>
+					<Paper className={classes.paper}></Paper>
+				</Grid>
+				<Grid item xs={4}>
+					<Paper className={classes.paper}></Paper>
+				</Grid>
+			</React.Fragment>
+		)
+	}
+
+	return (
+		<div className={classes.root}>
+			<Typography variant='h1'>Tic-Tac-Toe</Typography>
+			<Grid container spacing={0}>
+				<Grid container item xs={12} spacing={0}>
+					<FormRow />
+				</Grid>
+				<Grid container item xs={12} spacing={0}>
+					<FormRow />
+				</Grid>
+				<Grid container item xs={12} spacing={0}>
+					<FormRow />
+				</Grid>
+			</Grid>
+		</div>
+	)
 }
-
-export default App;
